5f07fcb90273fc9920e9e915d877de3c3c56ae25
oumontiel <omarume@gmail.com>
2020-07-30 13:17:56 -0700

    Add Login servlet and make navbar buttons dependent on user login status

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -11,3 +11,61 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+function sayHi() {
+  console.log("hi");
+}
+
+function fetchLogin() {
+  fetch('/login').then(response => response.json()).then(user => {
+    // If user is logged in, show logout button in navbar.
+    if (user.loggedIn == true) {
+      console.log("logged in");
+      // Create logout button.
+      const logoutButton = document.createElement('button');
+      logoutButton.setAttribute('type', 'button');
+      logoutButton.classList.add('btn btn-outline-success');
+      logoutButton.innerHTML = 'Log Out';
+
+      // Create navbar item to hold logout button.
+      const logoutButtonNavbarItem = document.createElement('li');
+      logoutButtonNavbarItem.classList.add('nav-item');
+      logoutButtonNavbarItem.appendChild(logoutButton);
+    
+      // Append logout button to navbar.
+      const authenticationButtons = document.getElementById('authentication');
+      authenticationButtons = '';
+      authenticationButtons.appendChild(logoutButtonNavbarItem);
+    // If user is logged out, show signup and login buttons in navbar.
+    } else {
+      console.log("logged out");
+      // Create signup button.
+      const signupButton = document.createElement('button');
+      signupButton.setAttribute('type', 'button');
+      signupButton.classList.add('btn btn-success');
+      signupButton.innerHTML = 'Sign Up';
+
+      // Create navbar item to hold signup button.
+      const signupButtonNavbarItem = document.createElement('li');
+      signupButtonNavbarItem.classList.add('nav-item');
+      signupButtonNavbarItem.appendChild(signupButton);
+
+      // Create login button.
+      const loginButton = document.createElement('button');
+      loginButton.setAttribute('type', 'button');
+      loginButton.classList.add('btn btn-outline-success');
+      loginButton.innerHTML = 'Log In';
+
+      // Create navbar item to hold login button.
+      const loginButtonNavbarItem = document.createElement('li');
+      loginButtonNavbarItem.classList.add('nav-item');
+      loginButtonNavbarItem.appendChild(loginButton);
+
+      // Append signup and login buttons to navbar.
+      const authenticationButtons = document.getElementById('authentication');
+      authenticationButtons = '';
+      authenticationButtons.appendChild(signupButtonNavbarItem);
+      authenticationButtons.appendChild(loginButtonNavbarItem);
+    }
+  })
+}
