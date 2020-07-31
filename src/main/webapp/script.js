411671008ed0c5c15e8fbff09e0fd007e839028f
Mariano Shaar <a00825287@itesm.mx>
2020-07-31 14:29:27 -0500

    Add fetchForum function

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -11,3 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+function fetchForum() {
+  fetch('/fetch-forum').then(response => response.json()).then(data => console.log(data));
+}
