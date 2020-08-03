4592f742fe837532706e708989da543bcf2edae9
Mariano Shaar <a00825287@itesm.mx>
2020-08-03 18:20:01 -0500

    Add number of answers to query result in servlet

--- src/main/java/com/google/sps/servlets/FetchForumServlet.java
+++ src/main/java/com/google/sps/servlets/FetchForumServlet.java
@@ -68,6 +68,7 @@ public class FetchForumServlet extends HttpServlet {
             question.setAskerName(queryResult.getString(8));
             question.setDateTime(queryResult.getTimestamp(5));
             question.setNumberOfFollowers(queryResult.getInt(7));
+            question.setNumberOfAnswers(queryResult.getInt(11));
 
             questions.add(question);
           }
