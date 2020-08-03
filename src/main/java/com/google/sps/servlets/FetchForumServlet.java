ee575d844f7e0381fe154c7a3b9a7789f3ab1564
Mariano Shaar <a00825287@itesm.mx>
2020-08-03 18:58:31 -0500

    Split query string

--- src/main/java/com/google/sps/servlets/FetchForumServlet.java
+++ src/main/java/com/google/sps/servlets/FetchForumServlet.java
@@ -44,9 +44,11 @@ public class FetchForumServlet extends HttpServlet {
   String user = "root";
   String password = "";
 
-  // This is the query that will be executed.
-  String query = "SELECT * FROM Question LEFT JOIN (SELECT question_id, COUNT(follower_id) followers FROM QuestionFollower GROUP BY question_id) CountTable ON Question.id=CountTable.question_id LEFT JOIN (SELECT name, id AS asker_id FROM User) NameTable ON Question.asker_id=NameTable.asker_id LEFT JOIN (SELECT question_id, COUNT(id) answers FROM Answer GROUP BY question_id) AnswerTable ON Question.id=AnswerTable.question_id;";
 
+
+  // This is the query that will be executed.
+  String query;
+  
   // This is the list that will hold all the questions from the query.
   List<QuestionObject> questions = new ArrayList<>();
   
@@ -55,6 +57,19 @@ public class FetchForumServlet extends HttpServlet {
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  query = "SELECT * FROM Question ";
+  query = query.concat("LEFT JOIN ");
+  query = query.concat("(SELECT question_id, COUNT(follower_id) followers FROM QuestionFollower ");
+  query = query.concat("GROUP BY question_id) CountTable ");
+  query = query.concat("ON Question.id=CountTable.question_id ");
+  query = query.concat("LEFT JOIN ");
+  query = query.concat("(SELECT name, id AS asker_id FROM User) NameTable ");
+  query = query.concat("ON Question.asker_id=NameTable.asker_id ");
+  query = query.concat("LEFT JOIN ");
+  query = query.concat("(SELECT question_id, COUNT(id) answers FROM Answer ");
+  query = query.concat("GROUP BY question_id) AnswerTable ");
+  query = query.concat("ON Question.id=AnswerTable.question_id;");
+
     // The connection and query are attempted.
     try (Connection connection = DriverManager.getConnection(url, user, password);
         PreparedStatement preparedStatement = connection.prepareStatement(query);
