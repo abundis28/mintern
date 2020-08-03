f6a5f14535c142c1c1141a62fd53cd3526ae4634
Mariano Shaar <a00825287@itesm.mx>
2020-08-03 14:14:01 -0500

    Change style nit

--- tests/java-db-test/src/main/webapp/script.js
+++ tests/java-db-test/src/main/webapp/script.js
@@ -1,4 +1,4 @@
 // This function will show ids and names of all users in the database in the console.
 function db() {
-    fetch('/test').then(response => response.json()).then(data => console.log(data));
+  fetch('/test').then(response => response.json()).then(data => console.log(data));
 }
