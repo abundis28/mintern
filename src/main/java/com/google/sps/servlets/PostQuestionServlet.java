abdcad12832221590b6683e314a156a209d08ebd
Mariano Shaar <a00825287@itesm.mx>
2020-08-05 14:59:24 -0500

    Change import order

--- src/main/java/com/google/sps/servlets/PostQuestionServlet.java
+++ src/main/java/com/google/sps/servlets/PostQuestionServlet.java
@@ -25,8 +25,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
