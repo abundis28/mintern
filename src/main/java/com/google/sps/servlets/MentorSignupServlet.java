5e2de28436b63c5167d3d4585fc82c68fe101395
Omar Montiel <omarume@gmail.com>
2020-08-06 10:54:19 -0700

    Add doGet() function to MentorSignupServlet to return subject tags from database

--- src/main/java/com/google/sps/servlets/MentorSignupServlet.java
+++ src/main/java/com/google/sps/servlets/MentorSignupServlet.java
@@ -16,9 +16,12 @@ package com.google.sps.servlets;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.sps.classes.SubjectTag;
 import com.google.sps.classes.Utility;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.annotation.WebServlet;
@@ -32,6 +35,46 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/mentor-signup")
 public class MentorSignupServlet extends HttpServlet {
 
+  // Set up variables needed to connect to MySQL database.
+  String url = Utility.SQL_URL;
+  String user = Utility.SQL_USER;
+  String password = Utility.SQL_PASSWORD;
+
+  /**
+   * Gets mentor experience tags from database and returns as JSON.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Set up list to store subject tags.
+    List<SubjectTag> subjectTags = new ArrayList<SubjectTag>();
+    
+    // Set up query to retrieve all subject tags.
+    String query = "SELECT * FROM SubjectTag";
+
+    try {
+      // Establish connection to MySQL database.
+      Connection connection = DriverManager.getConnection(url, user, password);
+
+      // Create the MySQL prepared statement, execute it, and store the result.
+      PreparedStatement preparedStatement = connection.prepareStatement(query);
+      ResultSet queryResult = preparedStatement.executeQuery();
+
+      // Store queryResult in map and close connection to database.
+      while (queryResult.next()) {
+        subjectTags.add(new SubjectTag(
+            queryResult.getInt(1), queryResult.getString(2), queryResult.getString(3)));
+      }
+      connection.close();
+    } catch (SQLException exception) {
+      // If the connection or the query don't go through, get the log of the error.
+      Logger logger = Logger.getLogger(MentorSignupServlet.class.getName());
+      logger.log(Level.SEVERE, exception.getMessage(), exception);
+    }
+
+    response.setContentType("application/json");
+    response.getWriter().println(Utility.convertToJsonUsingGson(subjectTags));
+  }
+  
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     UserService userService = UserServiceFactory.getUserService();
@@ -44,11 +87,6 @@ public class MentorSignupServlet extends HttpServlet {
     int major = Integer.parseInt(request.getParameter("major"));
     String[] experienceTags = request.getParameterValues("experience");
     Boolean is_mentor = true;
-
-    // Set up variables needed to connect to MySQL database.
-    String url = "jdbc:mysql://localhost:3306/Mintern?useSSL=false&serverTimezone=PST8PDT";
-    String user = "root";
-    String password = "";
 
     // Set up query to insert new user into database.
     String query = "INSERT INTO User (first_name, last_name, username, email, major_id, is_mentor) "
@@ -90,11 +128,11 @@ public class MentorSignupServlet extends HttpServlet {
       preparedStatement.setInt(2, Integer.parseInt(tag));
       preparedStatement.execute();
       connection.close();
-    } catch (SQLException exception) {
-      // If the connection or the query don't go through, get the log of the error.
-      Logger logger = Logger.getLogger(MentorSignupServlet.class.getName());
-      logger.log(Level.SEVERE, exception.getMessage(), exception);
-    }
+      } catch (SQLException exception) {
+        // If the connection or the query don't go through, get the log of the error.
+        Logger logger = Logger.getLogger(MentorSignupServlet.class.getName());
+        logger.log(Level.SEVERE, exception.getMessage(), exception);
+      }
     }
 
     response.sendRedirect("/index.html");
