ab2e8165c16b27537335542acedf01754b6ba0dc
Mariano Shaar <a00825287@itesm.mx>
2020-08-11 17:24:54 -0500

    Import maps

--- src/main/java/com/google/sps/servlets/FetchAnswersServlet.java
+++ src/main/java/com/google/sps/servlets/FetchAnswersServlet.java
@@ -24,6 +24,8 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
