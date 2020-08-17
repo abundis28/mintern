346625d97d0877e34f24e0ecab01dec7a57bc48f
Omar Montiel <omarume@gmail.com>
2020-08-17 11:40:31 -0700

    Fix errors in SqlConstants.java

--- src/main/java/com/google/sps/classes/SqlConstants.java
+++ src/main/java/com/google/sps/classes/SqlConstants.java
@@ -18,29 +18,6 @@ package com.google.sps.classes;
  * SQL constants used throughout different queries.
  */
 public final class SqlConstants {
-<<<<<<< HEAD
-=======
-  // Constants used for querying a question.
-  public static final int QUESTION_FETCH_ID = 1;
-  public static final int QUESTION_FETCH_TITLE = 2;
-  public static final int QUESTION_FETCH_BODY = 3;
-  public static final int QUESTION_FETCH_ASKERID = 4;
-  public static final int QUESTION_FETCH_ASKERNAME = 8;
-  public static final int QUESTION_FETCH_DATETIME = 5;
-  public static final int QUESTION_FETCH_NUMBEROFFOLLOWERS = 7;
-  public static final int QUESTION_FETCH_NUMBEROFANSWERS = 11;
-
-  // Constants used for inserting a question.
-  public static final int QUESTION_INSERT_TITLE = 1;
-  public static final int QUESTION_INSERT_BODY = 2;
-  public static final int QUESTION_INSERT_ASKERID = 3;
-
-  // Constants used for inserting a follower.
-  public static final int QUESTION_FETCH_MAXID = 1;
-  public static final int FOLLOWER_INSERT_QUESTIONID = 1;
-  public static final int FOLLOWER_INSERT_ASKERID = 2;
-
->>>>>>> 2116dc2f73cf0360459e4ba0dc43f494404b07f6
   // Constants used for querying a user.
   public static final int USER_FETCH_ID = 1;
   
@@ -51,10 +28,6 @@ public final class SqlConstants {
   public static final int USER_INSERT_EMAIL = 4;
   public static final int USER_INSERT_MAJOR = 5;
   public static final int USER_INSERT_ISMENTOR = 6;
-
-  // Constants used for inserting mentor experience.
-  public static final int MENTOR_EXPERIENCE_INSERT_ID = 1;
-  public static final int MENTOR_EXPERIENCE_INSERT_TAG = 2;
 
   // Constants to update mentor evidence.
   public static final int MENTOR_EVIDENCE_PARAGRAPH = 1;
@@ -111,6 +84,7 @@ public final class SqlConstants {
   public static final int MENTOR_APPROVAL_INSERT_APPROVERID = 2;
 
   // Constants used for querying a question.
+  public static final int QUESTION_FETCH_ID = 1;
   public static final int QUESTION_FETCH_TITLE = 2;
   public static final int QUESTION_FETCH_BODY = 3;
   public static final int QUESTION_FETCH_ASKERID = 4;
